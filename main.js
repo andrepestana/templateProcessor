b204e6e634795f5d610e4af2729d0b67d8432981
apestana <apestana@pacificgeotech.com>
2020-01-03 17:21:12 -0800

    implemented recursiveness to traverse through dir tree

--- main.js
+++ main.js
@@ -1,28 +1,64 @@
-var fs = require('fs')
+var path = require('path'),
+    fs = require('fs');
 
 const CONFIG_FILE = 'config/general.json';
 const REPLACEMENTS_FILE = 'config/replacements.json';
 
 let generalConfig = JSON.parse(fs.readFileSync(CONFIG_FILE));
+let replacements = JSON.parse(fs.readFileSync(REPLACEMENTS_FILE));
 
-fs.readFile(generalConfig.template.fileName, generalConfig.template.charEncoding, function (err, data) {
-  if (err) {
-    return console.log(err);
+
+function checkDir(basePath) {
+
+  fs.readdir(basePath, function(err, items) {
+    for (var i=0; i<items.length; i++) {
+      if(is_dir(basePath +'/'+ items[i])) {
+        checkDir(basePath +'/'+ items[i]);
+      } else {
+        applyReplacements(basePath +'/'+ items[i]);
+      }
+    }
+  });
+}
+
+checkDir(generalConfig.templateFolder.path);
+
+function applyReplacements(file) {
+  fs.readFile(file, generalConfig.template.charEncoding, function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    let result = data;
+    Object.keys(replacements).forEach(function (item) {
+      let re = new RegExp(item,"g");
+      result = result.replace(re, replacements[item]);
+    });
+    ensureDirectoryExistence(generalConfig.resultFolder.path + '/' + generalConfig.result.prefix + file + generalConfig.result.suffix);
+    fs.writeFile(generalConfig.resultFolder.path + '/' + generalConfig.result.prefix + file + generalConfig.result.suffix, 
+                result, 
+                generalConfig.template.charEncoding, 
+                function (err) {
+      if (err) return console.log(err);
+    });
+  });
+}
+
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
   }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
 
-  let replacements = JSON.parse(fs.readFileSync(REPLACEMENTS_FILE));
-
-  let result = data;
-  Object.keys(replacements).forEach(function (item) {
-    let re = new RegExp(item,"g");
-    result = result.replace(re, replacements[item]);
-  });
-    
-  fs.writeFile(generalConfig.result.prefix + generalConfig.template.fileName + generalConfig.result.suffix, 
-               result, 
-               generalConfig.template.charEncoding, 
-               function (err) {
-     if (err) return console.log(err);
-  });
-
-});
+function is_dir(path) {
+  try {
+      var stat = fs.lstatSync(path);
+      return stat.isDirectory();
+  } catch (e) {
+      // lstatSync throws an error if path doesn't exist
+      return false;
+  }
+}
